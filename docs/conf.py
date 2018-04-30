631a761cbbfe690080e040bf3e1563c9a3344b25
Samuel <samuel.weber@normalesup.org>
2018-04-30 14:19:33 +0200

    maj conf.py to convert notebook
    maj conf.py
    
    mv converter
    
    add ipynb_to_gallery converter
    
    remove unecessary file

--- docs/conf.py
+++ docs/conf.py
@@ -14,10 +14,6 @@
 #
 # import os
 import sys
-<<<<<<< HEAD
-import sphinx_nbexamples
-=======
->>>>>>> e4cb41d... WIP
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -46,9 +42,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
-    # 'nbsphinx',
     'sphinx_gallery.gen_gallery',
-    'sphinx_nbexamples',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -195,12 +189,49 @@ intersphinx_mapping = {
 
 sphinx_gallery_conf = {
     # path to your examples scripts
-    'examples_dirs': '../examples_plot',
+    'examples_dirs': '../examples_code',
     # path where to save gallery generated examples
-    'gallery_dirs': 'auto_examples_plot',
+    'gallery_dirs': 'auto_examples_code',
     'backreferences_dir': False,
-    # 'reference_url': {
-    #     'sphinx_gallery': None,
-    # }
+    'filename_pattern': '',
+    'reference_url': {
+        'sphinx_gallery': None,
+    }
 }
 
+import glob
+import pypandoc as pdoc
+import json
+
+def convert_ipynb_to_gallery(file_name):
+    python_file = ""
+
+    nb_dict = json.load(open(file_name))
+    cells = nb_dict['cells']
+
+    for i, cell in enumerate(cells):
+        if i == 0:  
+            assert cell['cell_type'] == 'markdown', \
+                'First cell has to be markdown'
+
+            md_source = ''.join(cell['source'])
+            rst_source = pdoc.convert_text(md_source, 'rst', 'md')
+            python_file = '"""\n' + rst_source + '\n"""'
+        else:
+            if cell['cell_type'] == 'markdown':
+                md_source = ''.join(cell['source'])
+                rst_source = pdoc.convert_text(md_source, 'rst', 'md')
+                commented_source = '\n'.join(['# ' + x for x in
+                                              rst_source.split('\n')])
+                python_file = python_file + '\n\n\n' + '#' * 70 + '\n' + \
+                    commented_source
+            elif cell['cell_type'] == 'code':
+                source = ''.join(cell['source'])
+                python_file = python_file + '\n' * 2 + source
+
+    python_file = python_file.replace("\n%", "\n# %")
+    open(file_name.replace('.ipynb', '.py'), 'w').write(python_file)
+    return python_file
+
+for fname in glob.glob(sphinx_gallery_conf["examples_dirs"]+"/**/*ipynb", recursive=True):
+    convert_ipynb_to_gallery(fname)
